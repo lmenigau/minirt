c324c5d31d7d469ade423d639fc1df90ada012a4
Margaux Riant <mriant@student.42.fr>
2022-10-20 16:11:51 +0200

    Norminette Ok,except header for the files:
     Changes to be committed:
    	modified:   mat_inv.c
    	modified:   parse_camcyl.c
    	modified:   sphere.c
    	modified:   transform.c

--- mat_inv.c
+++ mat_inv.c
@@ -4,9 +4,8 @@ void	inv_col1(t_mat4 *m, t_mat4 *id)
 {
 	if ((*m).x.x > 1e-6 || (*m).x.x < -1e-6)
 	{
-		// (*m).x.x = 1e-6;
-	(*id).x = mul4((*id).x, 1 / (*m).x.x);
-	(*m).x = mul4((*m).x, 1 / (*m).x.x);
+		(*id).x = mul4((*id).x, 1 / (*m).x.x);
+		(*m).x = mul4((*m).x, 1 / (*m).x.x);
 	}
 	if ((*m).y.x != 0)
 	{
@@ -29,10 +28,9 @@ void	inv_col1(t_mat4 *m, t_mat4 *id)
 void	inv_col2(t_mat4 *m, t_mat4 *id)
 {
 	if ((*m).y.y > 1e-6 || (*m).y.y < -1e-6)
-		// (*m).y.y = 1e-6;
 	{
-	(*id).y = mul4((*id).y, 1 / (*m).y.y);
-	(*m).y = mul4((*m).y, 1 / (*m).y.y);
+		(*id).y = mul4((*id).y, 1 / (*m).y.y);
+		(*m).y = mul4((*m).y, 1 / (*m).y.y);
 	}
 	if ((*m).z.y != 0)
 	{
@@ -50,10 +48,9 @@ void	inv_col2(t_mat4 *m, t_mat4 *id)
 void	inv_col3(t_mat4 *m, t_mat4 *id)
 {
 	if ((*m).z.z > 1e-6 || (*m).z.z < -1e-6)
-		// (*m).z.z = 1e-6;
 	{
-	(*id).z = mul4((*id).z, 1 / (*m).z.z);
-	(*m).z = mul4((*m).z, 1 / (*m).z.z);
+		(*id).z = mul4((*id).z, 1 / (*m).z.z);
+		(*m).z = mul4((*m).z, 1 / (*m).z.z);
 	}
 	if ((*m).w.z != 0)
 	{
@@ -73,10 +70,9 @@ void	inv_col3(t_mat4 *m, t_mat4 *id)
 void	inv_col4(t_mat4 *m, t_mat4 *id)
 {
 	if ((*m).w.w > 1e-6 || (*m).w.w < -1e-6)
-		// (*m).w.w = 1e-6;
 	{
-	(*id).w = mul4((*id).w, 1 / (*m).w.w);
-	(*m).w = mul4((*m).w, 1 / (*m).w.w);
+		(*id).w = mul4((*id).w, 1 / (*m).w.w);
+		(*m).w = mul4((*m).w, 1 / (*m).w.w);
 	}
 }
 
